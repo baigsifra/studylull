6319b9b86e51630e730bd2e624b6c1c566e5d942
baigsifra <84186625+baigsifra@users.noreply.github.com>
2022-08-06 19:12:07 -0500

    study page functional

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+const nav = document.getElementById('main-nav');
+let seconds = 0;
+let interval = null;
+
 function changeNav(name) {
-    const nav = document.getElementById('main-nav');
     const title = document.getElementById('title-heading');
     title.innerText = name;
+
+    hideAll();
+    document.getElementById(name).style.display = 'block';
 
     switch (name) {
         case ('home'):
@@ -20,7 +26,59 @@ function changeNav(name) {
             nav.setAttribute('class', 'deepchampagne');
             break;
         case ('study'):
-            nav.setAttribute('class', 'babypink');
+            nav.style.display = 'none';
+            start();
             break;
     }
+}
+
+function hideAll() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('breathe').style.display = 'none';
+    document.getElementById('puzzle').style.display = 'none';
+    document.getElementById('generator').style.display = 'none';
+    document.getElementById('music').style.display = 'none';
+    document.getElementById('study').style.display = 'none';
+}
+
+function goToBreak() {
+    nav.style.display = 'flex';
+    changeNav('home');
+    reset();
+}
+
+function timer() {
+    const timeDisplay = document.getElementById('timer');
+    
+    seconds++;
+
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+    let secs = seconds % 60;
+
+    if(secs < 10) {secs = '0' + secs;}
+    if(mins < 10) {mins = '0' + mins;}
+    if(hrs < 10) {hrs = '0' + hrs;}
+
+    timeDisplay.innerText = `${hrs}:${mins}:${secs}`;
+}
+
+function start() {
+    if(interval) {
+        return;
+    }
+
+    interval = setInterval(timer, 1000);
+}
+
+function stop() {
+    clearInterval(interval);
+    interval = null;
+}
+
+function reset() {
+    const timeDisplay = document.getElementById('timer');
+    stop();
+    seconds = 0;
+    timeDisplay.innerText = '00:00:00';
 }
